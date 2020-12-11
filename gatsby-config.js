628e458d86875bc035ca2208db223895ff0577f0
Denis Veleaev <veleaevdenis@gmail.com>
2020-12-11 18:02:51 +0200

    Add google analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -51,8 +51,11 @@ module.exports = {
         },
       },
     },
-    // this (optional) plugin enables Progressive Web App + Offline functionality
-    // To learn more, visit: https://gatsby.dev/offline
-    // `gatsby-plugin-offline`,
+    {
+      resolve: `gatsby-plugin-google-gtag`,
+      options: {
+        trackingIds: ['G-61L4GL4PK5'],
+      },
+    },
   ],
 }
